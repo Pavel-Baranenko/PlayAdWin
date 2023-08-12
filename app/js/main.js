70dcc94824585b2b57a6d0cf89a7d4e668f0fc72
Pavel-Baranenko <baranenko_work@mail.ru>
2023-08-12 22:43:10 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -29,36 +29,19 @@ var swiper = new Swiper(".swiper", {
 });
 
 
+var swiper1 = new Swiper(".mySwiper1", {
+  direction: "vertical",
+  loop: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  direction: "vertical",
+  loop: true,
 
+});
 const pageHeight = document.querySelector(".page__one").clientHeight;
 const go = document.querySelector('.go');
 const popup = document.querySelector('.popup');
 
-function playSound() {
-  var sound = document.getElementById("audio");
-  sound.play();
-}
-
-
-go.addEventListener('click', () => {
-  document.querySelector('.main').classList.add('move');
-  lastVideo.pause();
-  setTimeout(() => {
-    popup.classList.add('act');
-    playSound();
-  }, 4400);
-})
-
-
-
-
-
-// var vieValue = {
-//   slide1: 0,
-//   slide2: 0,
-//   slide3: 0,
-//   slide4: 0,
-// };
 
 
 const nextVideo = document.querySelector('.swiper-button-next');
@@ -112,7 +95,6 @@ document.querySelector('.balance__value').innerHTML = balance.toLocaleString() +
 const plus = document.querySelector('.plus');
 
 plus.addEventListener('click', () => {
-  // value
   if (balance > 0) {
     document.querySelector('.swiper-slide-active').querySelector('.video-views-value').innerHTML
     views = Number(document.querySelector('.swiper-slide-active').querySelector('.video-views-value').innerHTML) + 1000;
@@ -126,3 +108,95 @@ plus.addEventListener('click', () => {
   }
 
 })
+
+const Slides = document.querySelectorAll('.video-views-value');
+let rowsKol = 0
+videoViews = {
+}
+
+function playSound() {
+  var sound = document.getElementById("audio");
+  sound.play();
+}
+
+function videoSlides() {
+  let k = 0;
+  Slides.forEach((el) => {
+
+    if (Number(el.innerHTML) > 0) {
+      rowsKol += 1
+      videoViews['slide' + k] = Number(el.innerHTML);
+      k += 1;
+    }
+
+  });
+  videoViews['rowsKol'] = rowsKol;
+  if (videoViews.rowsKol == 1) {
+    document.querySelector('.slot-machine').classList.add('one-reel');
+    document.querySelector('.reels').classList.add('one-reel-animate');
+    setTimeout(() => {
+      popup.classList.add('act');
+      playSound();
+    }, 3800);
+
+  }
+  else if (videoViews.rowsKol == 2) {
+    document.querySelector('.slot-machine').classList.add('two-reel');
+    document.querySelector('.reels').classList.add('two-reel-animate');
+    setTimeout(() => {
+      document.querySelector('.first-reel  .mySwiper').scrollTop = 180;
+    }, 2000);
+    // setTimeout(() => {
+    //   popup.classList.add('act');
+    //   playSound();
+    // }, 4250);
+  };
+
+
+  for (var n = 0; n < rowsKol * 10; n++) {
+    const random = Math.floor(Math.random() * 100)
+    document.querySelector('.third-reel > .swiper-wrapper').innerHTML += ('<div class="swiper-slide">' + random + '</div>');
+  }
+  for (var m = 0; m < rowsKol * 10; m++) {
+    const random = Math.floor(Math.random() * 100)
+    document.querySelector('.last-reel > .swiper-wrapper').innerHTML += ('<div class="swiper-slide">' + random + '</div>');
+  }
+  for (var i = 0; i < rowsKol; i++) {
+    const slideNone = document.querySelector('.first-reel > .swiper-wrapper').querySelectorAll('.swiper-slide');
+    if ((videoViews['slide' + i]) > 0) {
+      document.querySelector('.second-reel > .swiper-wrapper').innerHTML += ('<div class="swiper-slide">' + (videoViews['slide' + i]) + '</div>');
+      slideNone[i].style.display = "flex";
+    }
+  }
+
+  // for (var v = 0; v < rowsKol; v++) {
+  //   let countReps = 0;
+  //   console.log(rowsKol);
+  //   if (Number(countReps) < Number(rowsKol)) {
+  //     setInterval(() => {
+  //       console.log("ASDFGHJK");
+  //       console.log(countReps);
+  //       countReps += 1;
+  //     }, 1000);
+  //   }
+  // }
+
+  // setTimeout(() => {
+  //   document.querySelector('.third-reel > .swiper-wrapper').style.marginTop =  ;
+  // }, 1000);
+}
+
+go.addEventListener('click', () => {
+  videoSlides()
+  if (videoViews.rowsKol > 0) {
+    document.querySelector('.main').classList.add('move');
+    lastVideo.pause();
+    // setTimeout(() => {
+    //   popup.classList.add('act');
+    //   playSound();
+    // }, 4400);
+  }
+
+})
+
+
